ccd0699b57cf05bbb6494fba380b149090505419
krugliashok08 <annakm803@gmail.com>
2024-03-24 13:14:00 +0300

    Lesson_11_maven

--- src/main/java/org/example/Factorial.java
+++ src/main/java/org/example/Factorial.java
@@ -1,19 +1,23 @@
 package org.example;
-
-/**
- * Hello world!
- *
- */
 public class Factorial
 {
     public static int Factorial( int number ) {
         if (number == 0) {
             return 1;
+        } else {
+            return number * Factorial(number - 1);
         }
-        int result = 1;
-        for (int i = 1; i <= number; i++) {
-            result *= i;
+    }
+    public static int FactorialCondition(int n, boolean includeOne) {
+        if (includeOne) {
+            return Factorial(n);
+        } else {
+            return Factorial(n-1);
         }
-        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(FactorialCondition(5, true));
+        System.out.println(FactorialCondition(5, false));
     }
 }
+
--- src/test/java/org/example/FactorialTest.java
+++ src/test/java/org/example/FactorialTest.java
@@ -3,11 +3,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FactorialTest {
-@Test
+    @Test
     public void testFactorial() {
-    assertEquals(1, Factorial.Factorial(0));
-    assertEquals(1, Factorial.Factorial(1));
-    assertEquals(2, Factorial.Factorial(2));
+        assertEquals(1, Factorial.Factorial(0));
+        assertEquals(1, Factorial.Factorial(1));
+        assertEquals(120, Factorial.Factorial(5));
+    }
+
+    @Test
+    void testFactorialWithCondition() {
+        assertEquals(120, Factorial.FactorialCondition(5, true));
+        assertEquals(24, Factorial.FactorialCondition(5, false));
+    }
 }
-}
+
+
 
