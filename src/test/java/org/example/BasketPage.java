35f6debd17129a2229f1298a3a5632c24b4167ec
Krugliashok08 <159140927+Krugliashok08@users.noreply.github.com>
2024-04-01 16:36:46 +0300

    Create BasketPage.java

---
+++ src/test/java/org/example/BasketPage.java
@@ -0,0 +1,21 @@
+package org.example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.Findby;
+import org.openqa.selenium.support.PageFactory;
+public class BasketPage {
+    WebDriver driver;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[4]/div/div[1]/form/div[1]/div[1]/div[2]/div/div/div/div/div/div/div[1]/div/a/span[1]")
+    private List<WebElement> productTitles;
+    FindBy(xpath = "//*[@id="app"]/div[4]/div/div[1]/form/div[1]/div[1]/div[2]/div/div/div/div/div/div/div[2]/div/div/input")
+    private List<WebElement> productQuantities;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[4]/div/div[1]/form/div[1]/div[1]/div[2]/div/div/div/div/div/div/div[1]/div/div[1]/div[2]")
+    private List<WebElement> productPrices;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[4]/div/div[1]/form/div[2]/div/div/div/div[3]/p/span[2]/span")
+    private WebElement totalPrice;
+    public CartPage(WebDriver driver) {
+           this.driver = driver;
+    PageFactory.initElements(driver, this);
+
+    }
+}
