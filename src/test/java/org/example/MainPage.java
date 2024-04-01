5ba927ed4b429429f449331d2b1edfaea8d43b9f
Krugliashok08 <159140927+Krugliashok08@users.noreply.github.com>
2024-04-01 16:34:26 +0300

    Update and rename MTC.java to MainPage.java

---
+++ src/test/java/org/example/MainPage.java
@@ -0,0 +1,23 @@
+package org.example;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+public class MainPage {
+    WEbDriver driver;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[4]/div/div[1]/form/div[1]/div[1]/div[1]/h1")
+    private List<WebElement> productTitles;
+    @FindBy(xpath = "//*[@id=\"8194f882-84fa-5e43-cf22-b0e340bd9356\"]/div[3]/div[11]/div[1]/div/div/button[1]")
+    private WebElement addToCartButton;
+
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void addToCart(int productIndex) {
+        productTitles.get(productIndex).click();
+        addToCartButton.click();
+    }
+}
