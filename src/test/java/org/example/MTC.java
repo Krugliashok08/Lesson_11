958a7f08a4dfba845c272ae2993c98c014c7fe04
Krugliashok08 <159140927+Krugliashok08@users.noreply.github.com>
2024-04-01 12:51:51 +0300

    Update MTC.java

--- src/test/java/org/example/MTC.java
+++ src/test/java/org/example/MTC.java
@@ -1,55 +1,57 @@
 package org.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.openqa.selenium.support.FindBy;
 public class TestMTC {
     WEbDriver driver;
-
-    @BeforeTest
+    @BeforeClass
     public void setUp() {
         WebDriverManager.chromedriver().clearDriverCach().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.mts.by/");
-        @Test
-        public void testBlock () {
-            driver.get("https://mts.by/");
-            WebElement blockTitleElement = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/h2"));
-            Assert.assertEquals("Онлайн пополнение без комиссии", blockTitleElement.getText());
-        }
-        @Test
-        public void testPaymentLogos () {
-            driver.get("https://mts.by/");
-            Assert.assertTrue(driver.findElements(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[2]"));
-        }
-        @Test
-        public void testReadMoreLink () {
-            driver.get("https://mts.by/");
-            WebElement readMoreLink = driver.findElement(By.linkText("Подробнее о сервисе"));
-            readMoreLink.click();
-            Assert.assertTrue(driver.getCurrentUrl().contains("detailed-service-info"));
-        }
-        @Test
-        public void testOnline () {
-            driver.get("https://mts.by/");
-            WebElement serviceInputField = driver.findElement(By.name("service"));
-            WebElement numberInputField = driver.findElement(By.name("number"));
-            serviceInputField.sendKeys("Услуги связи");
-            numberInputField.sendKeys("297777777");
-            WebElement continueButton = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[1]/div[2]"));
-            continueButton.click();
-            Assert.assertTrue(driver.getCurrentUrl().contains("online"));
-        }
-        @AfterTest
-        public void tearDown () {
-            driver.close();
+    }
+    @Test()
+    public void testBlock() {
+        driver.get("https://www.mts.by/");
+        servicesLink = driver.findElement(By.linkText("Услуги связи"));
+        servicesLink.click();
+        WebElement mobileLink = driver.findElement(By.linkText("Онлайн пополнение без комиссии"));
+        mobileLink.click();
+        WebElement phoneNumber = driver.findElement(By.id("connetion-phone"));
+        String phoneNumberPlaceholder = phoneNumber.getAttribute("placeholder");
+        Assert.assertEquals(phoneNumberPlaceholder, "Номер абонента");
+        WebElement amountField = driver.findElement(By.id("internet-phone"));
+        String amountPlaceholder = amountField.getAttribute("placeholder");
+        Assert.assertEquals(amountPlaceholder, "Сумма");
+        WebElement emailField = driver.findElement(By.id("internet-email"));
+        String emailPlaceholder = emailField.getAttribute("placeholder");
+        Assert.assertEquals(emailPlaceholder, "Email");
+
+        phoneNumberField.sendKeys("297777777");
+        amountField.sendKeys("10");
+        emailField.sendKeys("testng@example.com");
+        WebElement continueButton = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[1]/div[2]"));
+        continueButton.click();
+
+        WebElement visaIcon = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[1]/section/div/div[2]/ul/li[1]/img"));
+        WebElement mastercardIcon = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[1]/section/div/div[2]/ul/li[3]/img"));
+        WebElement belcartIcon = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[1]/section/div/div[2]/ul/li[5]/img"));
+        WebElement mirIcon = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[1]/section/div/div[2]/ul/li[6]/img"));
+
+        Assert.assertTrue(cardNumberField.isDisplayed());
+        Assert.assertTrue(visaIcon.isDisplayed());
+        Assert.assertTrue(mastercardIcon.isDisplayed());
+        Assert.assertTrue(belcartIcon.isDisplayed());
+        Assert.assertTrue(mirIcon.isDisplayed());
+    }
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
             driver.quit();
         }
     }
