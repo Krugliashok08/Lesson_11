4991f6318b6eec28fab8ae399720e0103dfe336a
Krugliashok08 <159140927+Krugliashok08@users.noreply.github.com>
2024-04-01 16:33:28 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        MTC mtc = PageFactory.initElements(driver,MTC.class);
-        driver.get("https://www.mts.by/");
+        MTC mtc = PageFactory.initElements(driver,Wildberries.class);
+        driver.get("https://www.wildberries.ru/");
     }
 }
