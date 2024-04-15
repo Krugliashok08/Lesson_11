08eb37581c446a833c039e4dba2faa9c496ff139
Krugliashok08 <159140927+Krugliashok08@users.noreply.github.com>
2024-04-15 12:09:12 +0300

    Update MainPage.java

--- src/test/java/org/example/MainPage.java
+++ src/test/java/org/example/MainPage.java
@@ -1,11 +1,12 @@
 package org.example;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import java.util.List;
+
 public class MainPage {
-    WEbDriver driver;
+    WebDriver driver;
     @FindBy(xpath = "//*[@id=\"app\"]/div[4]/div/div[1]/form/div[1]/div[1]/div[1]/h1")
     private List<WebElement> productTitles;
     @FindBy(xpath = "//*[@id=\"8194f882-84fa-5e43-cf22-b0e340bd9356\"]/div[3]/div[11]/div[1]/div/div/button[1]")
